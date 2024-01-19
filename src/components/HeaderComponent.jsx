1961a7d5a7823147a4bdce05aeea0d27852ce810
chyonee <chiwon99881@gmail.com>
2024-01-19 15:28:51 +0900

    Header home href changed

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -12,8 +12,8 @@ export default class HeaderComponent extends Component {
         <header>
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div>
-              <a href="http://www.fastcampus-dragon.net" className="navbar-brand">
-                kimdragon - jenkins
+              <a href="http://ecs.cwchoiit.net" className="navbar-brand">
+                cwcHoI-ECS
               </a>
             </div>
           </nav>
